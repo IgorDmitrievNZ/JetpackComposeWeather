a524a339b44211b024bbfe3b4a3d84c29153aee3
Igor Dmitriev <idmitrievnz@gmail.com>
2024-07-09 16:41:03 +1200

    Added loading screen

---
+++ app/src/main/java/com/example/android/jetpackcomposeweather/ui/componets/loading_screen/LoadingScreen.kt
@@ -0,0 +1,23 @@
+package com.example.android.jetpackcomposeweather.ui.componets.loading_screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(100.dp)
+        )
+    }
+}
