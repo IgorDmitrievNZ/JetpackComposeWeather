9f4f5b73c11bf90baa795ea14a10e3d73a9d71db
Igor Dmitriev <idmitrievnz@gmail.com>
2024-07-15 13:36:44 +1200

    Realisation of data store in view model
    The user can now save their favorite city
    When the app loaded the very first time Auckland is the favorite city by default

--- app/src/main/java/com/example/android/jetpackcomposeweather/ui/componets/main_screen/MainViewModel.kt
+++ app/src/main/java/com/example/android/jetpackcomposeweather/ui/componets/main_screen/MainViewModel.kt
@@ -5,11 +5,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.android.jetpackcomposeweather.datastore.StoreFavoriteCity
 import com.example.android.jetpackcomposeweather.network.model.WeatherDTO
 import com.example.android.jetpackcomposeweather.repository.Repository
 import com.example.android.jetpackcomposeweather.repository.model.City
-import com.example.android.jetpackcomposeweather.repository.model.WeatherModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
@@ -17,19 +19,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val repository: Repository
+    private val repository: Repository,
+    private val dataStoreFavoriteCity: StoreFavoriteCity
 ) : ViewModel() {
 
     var isNorth by mutableStateOf(true)
-    var favoriteCityName by mutableStateOf("Auckland")
-    var favoriteCityImage by mutableStateOf("https://lh6.googleusercontent.com/wVwTqqrvITsmOJFh7ZfkPVpPpLkerDh8aNLgX_Af4Qe42sLG6GpRWru7Xxb-Dqy1O1-lu39HWrPXzLW4hikiWZHi8D-b-9ZKkfgTc6cKHPf6iFdE04znARRBHWOXrR4eiDCV6QW2Fm5C7pIproYnOm_Z6hoRKnfhTBk_dVnK_kF7LTZsuPn6wMvr")
+    var favoriteCityName by mutableStateOf("")
+    var favoriteCityImage by mutableStateOf("")
     var favoriteDetailsUiState: FavoriteDetailsUiState by mutableStateOf(FavoriteDetailsUiState.Loading)
         private set
-
-    private val cityModel: WeatherModel? =
-        City.getNorthCities().find { item -> item.city.city == favoriteCityName }
-            ?: City.getSouthCities()
-                .find { item -> item.city.city == favoriteCityName }
 
     data class FavoriteCityModel(
         val temp: Int?,
@@ -38,13 +36,41 @@ class MainViewModel @Inject constructor(
     )
 
     init {
-        cityModel?.city?.let { getDetails(cityModel.city.lat, it.lon) }
+        //first to get city name from data store(Auckland is default when the App loads very firs time)
+        retrieveData()
+    }
+
+    private fun saveData(value: String) {
+        viewModelScope.launch {
+            dataStoreFavoriteCity.saveCityName(value)
+        }
+    }
+
+    private fun retrieveData() {
+        // get city name from data store (shared preferences)
+        dataStoreFavoriteCity.getCityName.onEach {
+            // find the actual city by name
+            val city = City.getNorthCities().find { item -> item.city.city == it }
+                ?: City.getSouthCities()
+                    .find { item -> item.city.city == it }
+            if (city != null) {
+                updateFavoriteCity(city.city)
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    // updates favorite city
+    private fun updateFavoriteCity(city: City) {
+        // update name and image
+        favoriteCityName = city.city
+        favoriteCityImage = city.image
+        // load details
+        getDetails(city.lat, city.lon)
     }
 
     fun onFavoriteToggle(city: City) {
-        favoriteCityName = city.city
-        favoriteCityImage = city.image
-        getDetails(city.lat, city.lon)
+        updateFavoriteCity(city)
+        saveData(city.city)
     }
 
     private fun getDetails(lat: Double, lon: Double) {
